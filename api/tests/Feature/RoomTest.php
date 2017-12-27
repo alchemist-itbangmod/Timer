5f97ec6729770a810afd98aa853285c0cdd0c16d
ImagineRabbits <mixxdarkz2@gmail.com>
2017-12-28 02:57:14 +0700

    [REMOVE] default test functions

--- api/tests/Feature/RoomTest.php
+++ api/tests/Feature/RoomTest.php
@@ -8,16 +8,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RoomTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample ()
-    {
-        $this->assertTrue(true);
-    }
-
     public function testGetApiRoomStatusShouldBe200 ()
     {
         $response = $this->get('/api/rooms');
