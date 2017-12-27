843370e51d74094bec1aaac16ed4e6cb9cf0e6ed
ImagineRabbits <mixxdarkz2@gmail.com>
2017-12-28 02:59:15 +0700

    [ADD] Room unit Test

--- api/tests/Unit/RoomTest.php
+++ api/tests/Unit/RoomTest.php
@@ -6,18 +6,19 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-use App\Repositories\RoomsRepository;
+use App\Repositories\RoomRepository;
 
 class RoomTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testGetRoomsRepositorySholdBeTrue ()
+    public function testClassRoomRepositoryShouldNotNull()
     {
-        $rooms = new RoomsRepository();
-        $this->assertEquals($rooms->get(), true);
+        $rooms = new RoomRepository;
+        $this->assertNotNull($rooms);
+    }
+
+    public function testGetRoomsRepositorySholdBeNotNull()
+    {
+        $rooms = new RoomRepository;
+        $this->assertNotNull($rooms->getAll());
     }
 }
