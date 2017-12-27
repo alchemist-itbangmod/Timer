c53244bc7baa0724f5529f443b8a9dfefc8f17d7
ImagineRabbits <mixxdarkz2@gmail.com>
2017-12-27 22:43:05 +0700

    [ADD] Database Migration v.1

--- api/database/migrations/2017_12_27_140026_create_rooms_table.php
+++ api/database/migrations/2017_12_27_140026_create_rooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoomsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->integer('role_id');
-            $table->rememberToken();
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('room_id');
+            $table->string('room_name');
+            $table->string('code_normal');
+            $table->string('code_admin');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 }
--- api/database/migrations/2017_12_27_140135_create_time_logger_table.php
+++ api/database/migrations/2017_12_27_140135_create_time_logger_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTimeLoggerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->integer('role_id');
-            $table->rememberToken();
+        Schema::create('time_logger', function (Blueprint $table) {
+            $table->increments('timelogger_id');
+            $table->integer('room_id');
+            $table->dateTime('time');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('time_logger');
     }
 }
